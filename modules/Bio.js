7f9bdf5f114714f6cc44e7cacbf95a1105d16be0
aidelojep <aidelojepeter123@gmail.com>
2022-01-12 13:22:38 +0100

    made changes to the style.css file and uploaded images

---
+++ modules/Bio.js
@@ -0,0 +1,12 @@
+const Bio = () => `
+<section class="bio">
+            <div class="profile-photo">
+                <img src="images/ig_profile_pic (Custom).jpeg" alt="profile-pic">
+            </div>
+            <div class="profile-info">
+                <p class="username">callmeperozon</p>
+                <button>follow</button>
+            </div>
+    </section>
+`
+export default Bio;
---
+++ modules/Gallery.js
@@ -0,0 +1,38 @@
+const Gallery = () => `
+<button>
+<i class="fas fa-plus-square"></i>
+<div class="gallery-nav">
+</div>
+<section class="gallery">
+        <a href="#" class="item">
+            <img src="https://images.pexels.com/photos/2055236/pexels-photo-2055236.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+        </a>
+        <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/984942/pexels-photo-984942.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/2249172/pexels-photo-2249172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/888894/pexels-photo-888894.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/9883880/pexels-photo-9883880.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/4395196/pexels-photo-4395196.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/300957/pexels-photo-300957.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/8474722/pexels-photo-8474722.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    <a href="#" class="item">
+        <img src="https://images.pexels.com/photos/1841840/pexels-photo-1841840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+    </a>
+    
+</section>
+`
+export default Gallery;
